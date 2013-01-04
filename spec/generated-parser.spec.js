c40a8f804e981b6c9a28aa5e4a7fc75fbfe07c1f
Ulric Wilfred <shaman.sir@gmail.com>
2013-01-04 13:04:23 +0100

    my context-level test

--- spec/generated-parser.spec.js
+++ spec/generated-parser.spec.js
@@ -1021,6 +1021,29 @@ describe("generated parser", function() {
       });
     });
 
+    describe("context levels", function() {
+
+      it("wraps every action in context", function() {
+        var parser = PEG.buildParser([
+              //          b    (              (                     d   c   )          a  dc   a   b   )    c     f    (                   z   )          z   f   c   b adcab
+              'start = a:"b" b:(a:"a" b:"b" c:(a:"c" c:"d" { return c + a; }) { return a + c + a + b; }) c:"c" d:"f" e:(d:"ez" { return d[1]; }) { return e + d + c + a + b; }'
+            ].join("\n"), options);
+
+        expect(parser).toParse("babcdcfez", "zfcbadcab");
+      });
+
+      it("wraps every rule reference in context", function() {
+        this.fail("NI");
+      });
+
+      it("wraps every choise in context", function() {
+        this.fail("NI");
+      });
+
+      // TODO: use collect-blocks as a reference for tests where nesting required
+
+    });
+
     /*
      * Following examples are from Wikipedia, see
      * http://en.wikipedia.org/w/index.php?title=Parsing_expression_grammar&oldid=335106938.
